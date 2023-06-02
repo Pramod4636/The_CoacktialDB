eb3fd77d4c1307abc611d27c8a96d14409584bbe
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-02 11:15:51 +0530

    home and abut page made

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+   <section className = "section about-section">
+     <h1 calssName = "section-title">About us </h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, dolorem? In, adipisci quis dicta ullam praesentium temporibus delectus vero quod facere beatae veritatis id, sequi eaque porro sint voluptate tempore.
+     Id iusto eius voluptatem. Suscipit placeat ipsam similique? Porro dolorum amet laudantium provident esse. Quasi quibusdam repudiandae modi voluptatum rerum, velit odio, veritatis, dolores explicabo recusandae reprehenderit illum vero iure.</p>
+   </section>
   )
 }
 
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,9 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div>
-      <h2>error page</h2>
-    </div>
+    <section calssName = "error-page section">
+      <div className ="error-container">
+        <h2>Opps! its A Dead End </h2>
+        <Link to="/" className = "btn">
+          Back to Home
+        </Link>
+      </div>
+    </section>
   )
 }
 
